c1369cfba107977ebb7f9c76e1083f534b66e770
James Udani <udani.jamescharlie@gmail.com>
2023-06-30 12:34:56 -0400

    first commit

---
+++ app/about/layout.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function AboutLayout({
+  children, // will be a page or nested layout
+}) {
+  return (
+    <section>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      <nav>
+        <Link href="/">Home</Link>
+      </nav>
+      {children} {children}
+    </section>
+  );
+}
